61e60729b2df5d6cf8e1214cb9baacaf13993488
ymasog <yer.masog@gmail.com>
2021-03-01 22:54:49 -0600

    added request onupgrade function for indexDB

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ['/', '/index.html', 'index.js', 'db.js', 'style.css'];
+const FILES_TO_CACHE = ['/', '/index.html', 'index.js', 'indexDB.js', 'style.css'];
 
 const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
