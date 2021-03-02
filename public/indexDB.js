4c6d35fc45d2c9c78714064d60a87b1bd887a56c
ymasog <yer.masog@gmail.com>
2021-03-01 22:55:21 -0600

    added on success and on error functions for indexdb

--- public/indexDB.js
+++ public/indexDB.js
@@ -7,3 +7,16 @@ request.onupgradeneeded = function (event) {
 	const db = event.target.result;
 	db.createObjectStore('pending', { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+	db = event.target.result;
+
+	if (navigator.onLine) {
+		checkDatabase();
+	}
+};
+
+request.onerror = function (event) {
+	// log error here
+	console.log('Error: ' + event.target.errorCode);
+};
