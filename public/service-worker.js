9fe810dbbce7fa1c86bd88a1cc77a713a8ffdf27
ymasog <yer.masog@gmail.com>
2021-03-01 23:29:29 -0600

    updated readme

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,12 @@
-const FILES_TO_CACHE = ['/', '/index.html', 'index.js', 'indexDB.js', 'style.css'];
-
-const CACHE_NAME = 'static-cache-v2';
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    'index.js',
+    'indexDB.js',
+    'styles.css',
+  ];
+  
+  const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 // install
